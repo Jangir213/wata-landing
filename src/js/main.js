33832ce31023ddecbbe8b6eda5fbe74361fdc848
jangir <jangirhus@gmail.com>
2017-01-02 19:43:18 +0200

    begin posts

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,36 @@
-(function(){
+(function () {
 
-  const promo = document.querySelector('#promo');
-  const nav = document.querySelector('.nav');
-  let promoHeight;
-  let navHeight;
+  window.addEventListener('load', () => {
+    mainWindow();
+    imgHeight();
+  });
 
-  function resizePromo () {
-    promoHeight = window.innerHeight;
-    navHeight = nav.clientHeight;
-    promo.style.height = promoHeight + 'px';
-  }
 
-  resizePromo();
-  window.addEventListener('resize', resizePromo);
- 
+  function mainWindow() {
+    const promo = document.querySelector('#promo');
+    const nav = document.querySelector('.nav');
+    let promoHeight;
+    let navHeight;
+
+    function resizePromo() {
+      promoHeight = window.innerHeight;
+      navHeight = nav.clientHeight;
+      promo.style.height = promoHeight + 'px';
+    }
+
+    resizePromo();
+    window.addEventListener('resize', resizePromo);
+  };
+
+
+  function imgHeight() {
+    const wideImg = document.querySelector('.posts__item--wide');
+    const smallImg = document.querySelector('.posts__left-col .posts__item img');
+    const heightSmallImg = parseFloat( getComputedStyle(smallImg).height ); 
+    const smallDiv = document.querySelector('.posts__left-col .posts__item');
+    const pdBotDiv = parseFloat( getComputedStyle(smallDiv).paddingBottom ); 
+    const height = heightSmallImg * 2 + pdBotDiv * 4;
+    wideImg.style.height = height + 'px';
+  };
+
 })();
